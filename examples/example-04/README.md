209f87efe3b6c96185b81227415d6d4583eafdda
Julien Roche <julien.roche@viseo.com>
2016-08-03 08:40:47 +0200

    A fourth example based on typescript

---
+++ examples/example-04/README.md
@@ -0,0 +1,15 @@
+Example of use of typings to install a module:
+
+> npm run typings -- install dt~nameofthelibrary --save --global
+
+
+And to search some types:
+* http://microsoft.github.io/TypeSearch/
+* https://github.com/typings/typings
+
+Or with the command:
+> npm run typings search -- --name angularprotractor
+
+
+For example, to install the typings of protractor:
+npm run typings -- install dt~angular-protractor --save --global
---
+++ examples/example-04/example_spec.js
@@ -0,0 +1,35 @@
+'use strict';
+var TIME_OUT = 10000;
+describe('angularjs homepage', function () {
+    it('should greet the named user', function () {
+        browser.get('http://www.angularjs.org');
+        element(by.model('yourName')).sendKeys('Julie');
+        var greeting = element(by.binding('yourName'));
+        expect(greeting.getText()).toEqual('Hello Julie!');
+    });
+    describe('todo list', function () {
+        var todoList;
+        beforeEach(function () {
+            browser.get('http://www.angularjs.org');
+            todoList = element.all(by.repeater('todo in todoList.todos'));
+        });
+        it('should list todos', function () {
+            expect(todoList.count()).toEqual(2);
+            expect(todoList.get(1).getText()).toEqual('build an angular app');
+        });
+        it('should add a todo', function () {
+            var addTodo = element(by.model('todoList.todoText'));
+            var addButton = element(by.css('[value="add"]'));
+            addTodo.sendKeys('write a protractor test');
+            addButton.click();
+            expect(todoList.count()).toEqual(3);
+            expect(todoList.get(2).getText()).toEqual('write a protractor test');
+        });
+        it('should wait until the video tags are shown', function () {
+            browser.wait(protractor.ExpectedConditions.presenceOf(element(by.css('.video-img'))), TIME_OUT);
+            var videos = element.all(by.css('.video-img'));
+            expect(videos.count()).toEqual(2);
+        });
+    });
+});
+//# sourceMappingURL=example_spec.js.map
